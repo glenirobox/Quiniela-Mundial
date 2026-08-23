1140c4ff7fb5632e802d2c754db86f5a02b1041d
Glen Villagra <glengervir0422@gmail.com>
2026-08-23 13:12:33 -0600

    limpieza temporal

--- src/SistemaQuinielas/obj/Debug/net10.0-windows/SistemaQuinielas.AssemblyInfo.cs
+++ src/SistemaQuinielas/obj/Debug/net10.0-windows/SistemaQuinielas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaQuinielas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4a9615a13da35a6e774b7409d55392e5bd41c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9160b51677d092d831f9f837bb67f7f32631b9d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaQuinielas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaQuinielas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
