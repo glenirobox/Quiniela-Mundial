6e17c92c8c24aa217061096a6145966b87c60487
Glen Villagra <glengervir0422@gmail.com>
2026-08-25 17:55:09 -0600

    Agregada vista PosicionesComponent con calculo automatico de puntos

--- src/ProyectoTecnicasProgra/ProyectoTecnicasProgra/obj/Debug/net10.0/ProyectoTecnicasProgra.AssemblyInfo.cs
+++ src/ProyectoTecnicasProgra/ProyectoTecnicasProgra/obj/Debug/net10.0/ProyectoTecnicasProgra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoTecnicasProgra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e776e0853ea79ea1dbdf712a8ab92ac43917ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb102236b1f3d1a61356dd715a6c25b455c2211e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoTecnicasProgra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoTecnicasProgra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
