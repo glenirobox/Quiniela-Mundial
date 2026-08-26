e6c86a473d58d803614d5aed19d2017419ecc31c
Glen Villagra <glengervir0422@gmail.com>
2026-08-26 00:46:56 -0600

    Agregada vista de Mantenimiento de Usuarios para Administrador

--- src/ProyectoTecnicasProgra/ProyectoTecnicasProgra/obj/Debug/net10.0/ProyectoTecnicasProgra.AssemblyInfo.cs
+++ src/ProyectoTecnicasProgra/ProyectoTecnicasProgra/obj/Debug/net10.0/ProyectoTecnicasProgra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoTecnicasProgra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12430e254a59c5a2a5c61743765927d83e32cd0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbcc253f2740f536c0a9b642efe5e1965abfef53")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoTecnicasProgra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoTecnicasProgra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
